2c980257f3fb5a6abfc5faaa4ed37539e1ea313f
Rohan Nelson <rohan.nelson@gmail.com>
2023-01-04 12:40:53 +0800

    Monorepo setup!

---
+++ shared-components/index.js
@@ -0,0 +1,7 @@
+export { default as PolicyDocLayout } from './layouts/PolicyDocLayout.astro'
+export { default as BaseLayout } from './layouts/BaseLayout.astro'
+export { default as DocControlTable } from './components/DocControlTable.astro'
+export { default as SidebarToggle } from './components/SidebarToggle.astro'
+export { default as Header } from './components/Header.astro'
+export { default as TableofContents } from './components/TableofContents.astro'
+export { default as VersionControlTable } from './components/VersionControlTable.astro'
