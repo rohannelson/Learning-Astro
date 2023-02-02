ae04235f3218229bfb3e10b722cb3e76ff0e1058
Rohan Nelson <rohan.nelson@gmail.com>
2023-02-02 20:15:56 +0800

    moved global stylesheet to shared-components

--- shared-components/index.js
+++ shared-components/index.js
@@ -1,6 +1,7 @@
-export { default as BaseLayout } from './layouts/BaseLayout.astro'
+/*export { default as BaseLayout } from './layouts/BaseLayout.astro'
 export { default as DocControlTable } from './components/DocControlTable.astro'
 export { default as SidebarToggle } from './components/SidebarToggle.astro'
 export { default as Header } from './components/Header.astro'
 export { default as TableofContents } from './components/TableofContents.astro'
-export { default as VersionControlTable } from './components/VersionControlTable.astro'
+export { default as VersionControlTable } from './components/VersionControlTable.astro'*/
+export { default as globalStyles } from './components/global.css'
