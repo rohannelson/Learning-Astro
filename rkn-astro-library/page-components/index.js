46718aa8b0a20cc15586db9ff373d0969eb6b01f
Rohan Nelson <rohan.nelson@gmail.com>
2023-02-08 14:58:58 +0800

    all the changes... (including consolidating header and footer library into one base library and moving global styles there)

--- rkn-astro-library/page-components/index.js
+++ rkn-astro-library/page-components/index.js
@@ -1,3 +1,2 @@
 export { default as HeroImage } from './HeroImage.astro'
 export { default as SingleColumnBlock } from './SingleColumnBlock.astro'
-export { default as globalStyles } from './global.css'
