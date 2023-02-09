94052ee94fd7df6ec287324fc61a54b9d943c277
Rohan Nelson <rohan.nelson@gmail.com>
2023-02-09 20:42:29 +0800

    Added Google maps embed and made it responsive.

--- rkn-astro-library/page-components/index.js
+++ rkn-astro-library/page-components/index.js
@@ -1,2 +1,3 @@
 export { default as HeroImage } from './HeroImage.astro'
 export { default as SingleColumnBlock } from './SingleColumnBlock.astro'
+export { default as GoogleMaps } from './GoogleMaps.astro'
