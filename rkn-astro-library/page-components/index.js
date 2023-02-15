143d76fb7c998b6a5da50cc5a92f1f90b0e64b65
Rohan Nelson <rohan.nelson@gmail.com>
2023-02-15 22:39:22 +0800

    Work done on content blocks and home page

--- rkn-astro-library/page-components/index.js
+++ rkn-astro-library/page-components/index.js
@@ -1,4 +1,7 @@
 export { default as HeroImage } from './HeroImage.astro'
-export { default as SingleColumnBlock } from './SingleColumnBlock.astro'
+export { default as BlockSingleColumn } from './BlockSingleColumn.astro'
 export { default as GoogleMaps } from './GoogleMaps.astro'
 export { default as HeroBanner } from './HeroBanner.astro'
+export { default as BlockDoubleColumn } from './BlockDoubleColumn.astro'
+export { default as SectionWrapper } from './SectionWrapper.astro'
+export { default as BlockPaddingWrapper } from './BlockPaddingWrapper.astro'
